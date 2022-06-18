cae2c2a8dc97bdde9b374fa0609aef41279bfe34
Ben Napper <benjamin.napper17@gmail.com>
2022-06-17 21:51:34 -0400

    link to home page

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -11,11 +11,13 @@ function Nav() {
             <ul className="link_list">
                 <li><Link to='/Radio' className='link'>Radio Broadcasts</Link></li>
                 <li><Link to='/Service' className='link'>Church Services</Link></li>
+                <li><a href="https://htsl.netlify.app" className='link'>Home Page</a></li>
                 {/* <li>News Letters</li> */}
             </ul>
             <ul className="link_list_small">
                 <li><Link to='/Radio' className='link'>Radio</Link></li>
                 <li><Link to='/Service' className='link'>Services</Link></li>
+                <li><a href="https://htsl.netlify.app" className='link'>Home Page</a></li>
                 {/* <li>Letters</li> */}
             </ul>
         </nav>
